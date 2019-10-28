e9f553f86c6fa84e7762f76e3ed46a6ebe26fd1f
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2019-10-28 01:18:25 -0700

    update crime and communities registration

--- src/EBayesDatasets.jl
+++ src/EBayesDatasets.jl
@@ -31,14 +31,7 @@ module EBayesDatasets
 
     #Load MovieLens.jl
     include("movielens.jl")
+    include("crime_and_communities.jl")
+    ##
 
-
-    #function __init__()
-    #    register(DataDep(
-    #      "communities-and-crime",
-    #      "https://archive.ics.uci.edu/ml/datasets/Communities+and+Crime+Unnormalized",
-    #      "https://archive.ics.uci.edu/ml/machine-learning-databases/00211/CommViolPredUnnormalizedData.txt",
-    #      "383a9530b2802b4457986095b13af8f02a802a2504feaef45783f9c129e003f1"
-    #    ))
-    #end
 end # module
--- src/crime_and_communities.jl
+++ src/crime_and_communities.jl
@@ -1,6 +1,13 @@
-register(DataDep(
-  "communities-and-crime",
-  "https://archive.ics.uci.edu/ml/datasets/Communities+and+Crime+Unnormalized",
-  "https://archive.ics.uci.edu/ml/machine-learning-databases/00211/CommViolPredUnnormalizedData.txt",
-  "383a9530b2802b4457986095b13af8f02a802a2504feaef45783f9c129e003f1"
-))
+function __init__()
+     register(DataDep(
+       "communities-and-crime",
+       "https://archive.ics.uci.edu/ml/datasets/Communities+and+Crime+Unnormalized",
+       "https://archive.ics.uci.edu/ml/machine-learning-databases/00211/CommViolPredUnnormalizedData.txt",
+       "383a9530b2802b4457986095b13af8f02a802a2504feaef45783f9c129e003f1"
+    ))
+end
+
+const crime_colnames_path = joinpath(dirname(@__FILE__), "..", "rawdata",
+                              "crime_and_communities","column_names.txt")
+
+
